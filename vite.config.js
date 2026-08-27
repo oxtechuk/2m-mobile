98dc952a006e39295f8b0f9bc93251cccb583569
Ox tech <oxtech.uk@gmail.com>
2026-08-27 12:35:30 +0200

    re

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-            //css
-            'resources/css/app.css',
-            "node_modules/glightbox/dist/css/glightbox.min.css",
-            'node_modules/quill/dist/quill.bubble.css',
-            'node_modules/quill/dist/quill.core.css',
-            'node_modules/quill/dist/quill.snow.css',
-            'node_modules/jsvectormap/dist/css/jsvectormap.min.css',
-            
-            //js
-            'resources/js/app.js',
-            'resources/js/pages/app-calendar.js',
-            'resources/js/pages/charts-apex.js',
-            'resources/js/pages/dashboard.js',
-            'resources/js/pages/form-editor.js',
-            'resources/js/pages/form-inputmask.js',
-            'resources/js/pages/gallery.js',
-            'resources/js/pages/maps-vector.js'
-        ],
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss()
